b6007c2443b5c9a18ab47ae0bfd94aa3cfad6ad8
Viktors Telle <viktors.telle@gmail.com>
2017-10-23 23:43:22 +0300

    Add project files.

---
+++ ECommerce.API/Model/ApiProduct.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.API.Model
+{
+    public class ApiProduct
+    {
+
+    }
+}
